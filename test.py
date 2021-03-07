d2a0757cea75b8be101beca0e8380191d62defd3
huangminggg <huangming.low@fundingsocieties.com>
2021-03-07 12:51:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,38 +3,44 @@ import pandas as pd
 from pmdarima.arima import auto_arima
 # from pypfopt.expected_returns import mean_historical_return
 from pypfopt.risk_models import CovarianceShrinkage
-
 from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt import risk_models
 from pypfopt import expected_returns
-import functools 
-import cvxpy as cp
+import functools
+
+
+def simple_moving_average(daily_return):
+    return daily_return[-50:].mean()*252
 
 
 def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings):
     nMarkets = CLOSE.shape[1]
     df = pd.DataFrame(CLOSE)
     df.rename(lambda x: settings['markets'][x], axis='columns', inplace=True)
-    daily_return = (np.log(df) - np.log(df.shift(1))).iloc[1:]
-    daily_return.drop(['CASH'], axis=1, inplace=True)
 
-    columns = list(daily_return)
-    mu = pd.Series()
-    mu = pd.Series([0], index=['CASH'])
+    # daily_return = df.pct_change().dropna(how="all")
+    # daily_return.drop(['CASH'], axis=1, inplace=True)
+    #
+    # columns = list(daily_return)
+    # mu = pd.Series()
+    # mu = pd.Series([0], index=['CASH'])
+    #
+        # for i in columns:
+        #     # fitting ARMA model first
+        #     # assume stationarity, d=0, no seasonality
+        #     res = simple_moving_average(daily_return[i])
+        #     # print(res)
+        #     # model = auto_arima(daily_return[i], start_p=1, start_q=1, max_p=5, max_q=5, d=0, seasonal=False, stationary=False,
+        #     #                suppress_warnings=True, error_action='warn', stepwise=True)
+        #     # pred = model.predict(n_periods=1)[0]
+        #     expected = pd.Series([res], index=[i])
+        #     mu = mu.append(expected)
 
-    for i in columns:
-        # fitting ARMA model first
-        # assume stationarity, d=0, no seasonality
-        model = auto_arima(daily_return[i], start_p=0, start_q=0, max_p=5, max_q=5, d=0, seasonal=False, stationary=True, suppress_warnings=True, error_action='warn', stepwise=True)
-        pred = model.predict(n_periods=1)[0]
-        expected = pd.Series([pred], index=[i])
-        mu = mu.append(expected)
-
-
-    # mu = expected_returns.ema_historical_return(df)
     df = df[list(filter(lambda x: x != 'CASH', settings['markets']))]
+    # mu = pd.Series([0], index=['CASH']).append(expected_returns.ema_historical_return(df, compounding=False))
+    mu = pd.Series([0], index=['CASH']).append(expected_returns.mean_historical_return(df, compounding=False))
+    mu.fillna(0, inplace=True)
     S = CovarianceShrinkage(df).ledoit_wolf()
-    # S = risk_models.sample_cov(df)
     # Add cash back into the covariance matrix
     S.insert(loc=0, column='CASH', value=0)
     cash = functools.reduce(lambda a, b: {**a, **b}, [{ticker: [0]} for ticker in settings['markets']])
@@ -52,8 +58,9 @@ def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, setting
             weights = settings['history'][-1]
         else:
             weights = np.array([1 if i == 'CASH' else 0 for i in settings['markets']])
+    # baseline
+    # weights = np.array([0 if i == 'CASH' else (1/88) for i in settings['markets']])
     settings['history'].append(weights)
-    weights = np.array([0 if i == 'CASH' else (1/88) for i in settings['markets']])
     return weights, settings
 
 
@@ -64,9 +71,9 @@ def mySettings():
     # futuresList = ['F_AD','F_BO','F_BP','F_C','F_CC','F_CD','F_CL','F_CT','F_DX','F_EC','F_ED','F_ES','F_FC','F_FV','F_GC','F_HG','F_HO','F_JY','F_KC','F_LB','F_LC','F_LN','F_MD','F_MP','F_NG','F_NQ','F_NR','F_O','F_OJ','F_PA','F_PL','F_RB','F_RU','F_S','F_SB','F_SF','F_SI','F_SM','F_TU','F_TY','F_US','F_W','F_XX','F_YM','F_AX','F_CA','F_DT','F_UB','F_UZ','F_GS','F_LX','F_SS','F_DL','F_ZQ','F_VX','F_AE','F_BG','F_BC','F_LU','F_DM','F_AH','F_CF','F_DZ','F_FB','F_FL','F_FM','F_FP','F_FY','F_GX','F_HP','F_LR','F_LQ','F_ND','F_NY','F_PQ','F_RR','F_RF','F_RP','F_RY','F_SH','F_SX','F_TR','F_EB','F_VF','F_VT','F_VW','F_GD','F_F']
 
     settings = {
-        'markets': ['CASH', 'F_AD', 'F_BO', 'F_BP','F_C','F_CC','F_CD','F_CL','F_CT'],
-        'lookback': 10, 'budget': 10 ** 6, 'slippage': 0.05,
-        'beginInSample': '20201001', 'endInSample': '20201231', 'history': [], 'invalid_solution': 0}
+        'markets': ['CASH','F_AD','F_BO','F_BP','F_C','F_CC','F_CD','F_CL','F_CT','F_DX','F_EC','F_ED','F_ES','F_FC','F_FV','F_GC','F_HG','F_HO','F_JY','F_KC','F_LB','F_LC','F_LN','F_MD','F_MP','F_NG','F_NQ','F_NR','F_O','F_OJ','F_PA','F_PL','F_RB','F_RU','F_S','F_SB','F_SF','F_SI','F_SM','F_TU','F_TY','F_US','F_W','F_XX','F_YM','F_AX','F_CA','F_DT','F_UB','F_UZ','F_GS','F_LX','F_SS','F_DL','F_ZQ','F_VX','F_AE','F_BG','F_BC','F_LU','F_DM','F_AH','F_CF','F_DZ','F_FB','F_FL','F_FM','F_FP','F_FY','F_GX','F_HP','F_LR','F_LQ','F_ND','F_NY','F_PQ','F_RR','F_RF','F_RP','F_RY','F_SH','F_SX','F_TR','F_EB','F_VF','F_VT','F_VW','F_GD','F_F'],
+        'lookback': 504, 'budget': 10 ** 6, 'slippage': 0.05,
+        'beginInSample': '20190101', 'endInSample': '20210225', 'history': [], 'invalid_solution': 0}
 
     return settings
 
@@ -74,5 +81,4 @@ def mySettings():
 # Evaluate trading system defined in current file.
 if __name__ == '__main__':
     import quantiacsToolbox
-    print(cp.installed_solvers())
     results = quantiacsToolbox.runts(__file__)
